7dd29b6fe336f786db5e40163a8aa3b45a5bf63a
ho8ae <xogh2242@gmail.com>
2024-10-08 10:07:57 +0900

    [#1]build: tailwindcss 빌드 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,45 +1,29 @@
-import { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../globals.css";
-import { Providers } from "./Providers";
-import { Suspense } from "react";
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { Suspense } from 'react'
+import {  Providers } from './Providers'
 
-export const metadata: Metadata = {
-  title: "Liveblocks",
-};
+const inter = Inter({ subsets: ['latin'] })
 
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "500", "600"],
-  display: "swap",
-});
+export const metadata = {
+  title: '퍼즐',
+  description: '프로젝트 개발 가이드라인을 제공하는 플랫폼',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={inter.className}>
-      <head>
-        <link
-          href="https://liveblocks.io/favicon-32x32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          href="https://liveblocks.io/favicon-16x16.png"
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        />
-      </head>
-      <body>
+    <html lang="en">
+      <body className={inter.className}>
         <Suspense>
-          <Providers>{children}</Providers>
+          <Providers>
+            {children}
+          </Providers>
         </Suspense>
-      </body>
+      </body> 
     </html>
-  );
+  )
 }

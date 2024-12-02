bb49d315b4474551e778d1007be42723377299b4
ho8ae <xogh2242@gmail.com>
2024-12-02 04:38:08 +0000

    [#10]refactor: 구글 로그인 리다이렉트 주소 변경

--- src/components/GoogleLogin/index.tsx
+++ src/components/GoogleLogin/index.tsx
@@ -5,7 +5,7 @@ import googleIcon from '~/images/logo/google-logo.svg';
 
 const GoogleLoginButton = () => {
   const handleGoogleLogin = () => {
-    window.location.href = 'http://kim-sun-woo.com:3000/auth/google/';
+    window.location.href = 'http://kim-sun-woo.com/auth/google/';
   };
 
   return (
@@ -17,15 +17,15 @@ const GoogleLoginButton = () => {
     >
       {/* Hover Effect Background */}
       <div className="absolute inset-0 rounded-lg bg-gray-50 opacity-0 transition-opacity duration-200 group-hover:opacity-100" />
-      
+
       {/* Content */}
       <div className="relative flex items-center justify-center space-x-3">
-        <Image 
-          src={googleIcon} 
-          alt="Google Logo" 
-          width={18} 
+        <Image
+          src={googleIcon}
+          alt="Google Logo"
+          width={18}
           height={18}
-          className="transition-transform duration-200 group-hover:scale-105" 
+          className="transition-transform duration-200 group-hover:scale-105"
         />
         <span className="text-[15px] font-medium text-gray-600 transition-colors duration-200 group-hover:text-gray-800">
           Continue with Google
